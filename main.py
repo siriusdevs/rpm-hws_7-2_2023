bf6b8614aa755d84385c32b0d8a454fe071d96fd
arina_pruzhinina <pruzininaa@mail.ru>
2023-03-11 14:53:48 +0300

    linters

---
+++ main.py
@@ -0,0 +1,19 @@
+"""Summator module."""
+
+
+def summator(fst: int, snd: int) -> int:
+    """
+    Get the sum of two values.
+
+    Args:
+        fst: int - first value
+        snd: int - second value
+
+    Returns:
+        int - result of summator
+    """
+    return fst + snd  # это Илья который слишком длинный для ванильного флейка только сейчас эта строка длинная
+
+
+if __name__ == '__main__':
+    print(summator(1, 8))
