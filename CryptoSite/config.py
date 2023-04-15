253d26e9b4d5d23fe36905510927625950fdf2ef
blinmakersha <valentinakulg@gmail.com>
2023-04-15 08:52:56 +0300

    Some changes in config and html.

--- CryptoSite/config.py
+++ CryptoSite/config.py
@@ -2,9 +2,9 @@ HOST = '127.0.0.1'
 PORT = 8003
 
 # TEMPLATES
-MAIN_TEMPLATE = 'index.html'
-COINS_TEMPLATE = 'coins.html'
-MARKET_OVERVIEW_TEMPLATE = 'market-overview.html'
+MAIN_TEMPLATE = 'cryptosite/templates/index.html'
+COINS_TEMPLATE = 'cryptosite/templates/coins.html'
+MARKET_OVERVIEW_TEMPLATE = 'cryptosite/templates/market-overview.html'
 
 # PAGES
 MARKET_OVERVIEW = '/overview'
